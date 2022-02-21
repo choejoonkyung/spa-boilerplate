46cd61f1ab5adbcd7c884a280ae377a25e2b0e2e
choejoonkyung <choejunkyung@gmail.com>
2022-02-21 17:03:42 +0900

    feat: http 통신 함수 연동

--- src/pages/ProductListPage/index.js
+++ src/pages/ProductListPage/index.js
@@ -1,17 +1,30 @@
 import ProductList from "../../components/Product/ProductList";
+import request from "../../utils/Service";
 
 function ProductListPage({ $target }) {
   const $page = document.createElement("article");
   $page.className = "product-list-page";
   $page.innerHTML = "<h2>상품리스트</h2>";
 
+  new ProductList({
+    $target: $page,
+    list: this.state,
+  });
+
   this.setState = (nextState) => {
     this.state = nextState;
+  };
+
+  this.fetchProducts = async () => {
+    const products = await request("/posts");
+    this.setState(products);
   };
 
   this.render = () => {
     $target.appendChild($page);
   };
+
+  this.fetchProducts();
 }
 
 export default ProductListPage;
